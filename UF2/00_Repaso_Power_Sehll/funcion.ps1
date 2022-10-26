c839aed34d889d99045780e5d582a93d0a1501bc
ssergivelass <111636899+ssergivelass@users.noreply.github.com>
2022-10-26 12:03:49 +0200

    Add files via upload

---
+++ UF2/00_Repaso_Power_Sehll/funcion.ps1
@@ -0,0 +1,28 @@
+﻿#trabajar con funciones.
+#Declaramos una funcion, la utilizaremos duespés cuando sea llamada.
+function RequestName {
+$Name = Read-Host "Entra tu nombre"
+Write-Output "Your name is: ${Name}"
+}
+Clear
+Get-Date
+#En este punto del codigo hacemos referencia a la funcion  que hemos declarado al pricipio del script.
+RequestName
+
+enum Ciudades
+{
+    Hannover
+    Madrid
+    Seattle
+    Barcelona
+    London
+    NewYork
+}
+
+function Seleccion-Ciudades
+{
+    param ([Ciudades][Parameter(Mandatory=$true)]$Ciudad)
+    Write-Host "Has elegido la ciudad de $Ciudad"
+}
+
+Seleccion-Ciudades 3
