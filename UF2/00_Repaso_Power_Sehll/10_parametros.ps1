9dc46dae9ed3aa2f8b152ddbed896637e0496f69
ssergivelass <111636899+ssergivelass@users.noreply.github.com>
2023-01-19 17:58:44 +0100

    Add files via upload

---
+++ UF2/00_Repaso_Power_Sehll/10_parametros.ps1
@@ -0,0 +1,14 @@
+﻿Param([string]$nombre,[string]$dia)
+
+#Write-Host "Hola $nombre, tienes $edad"
+
+if ($nombre -and $dia)
+{
+    Write-Host "OK!!!!! - Has introducido los parametros esperados"
+    Write-Host "Hola $nombre, hoy es $dia"  
+    Write-Host "El primer argumento es " $args[0]  
+}
+else
+{
+    Write-Host "No has introducido los parametros esperados."
+}
