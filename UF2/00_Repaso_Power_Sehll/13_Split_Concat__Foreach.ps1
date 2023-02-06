ae6e0bb54d3fd1590beba5ed255f362a4c577727
ssergivelass <111636899+ssergivelass@users.noreply.github.com>
2023-02-06 09:25:49 +0100

    Add files via upload

---
+++ UF2/00_Repaso_Power_Sehll/13_Split_Concat__Foreach.ps1
@@ -0,0 +1,14 @@
+﻿$message = "Numbers: "
+foreach($number in 1..10)
+{
+    $message += " $number"
+}
+Write-Host $message
+
+$lista = "uno,dos,tres"
+$msn = "Numbers: "
+foreach($nu in $lista -split ",")
+{
+    $msn += " $nu"
+}
+Write-Host $msn
