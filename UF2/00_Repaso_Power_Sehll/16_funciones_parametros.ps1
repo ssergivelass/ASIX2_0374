275328ad30de1e47adb3cca9bc3a7f9610c30528
m14 <111636899+ssergivelass@users.noreply.github.com>
2024-01-28 19:41:15 +0100

    Add files via upload

---
+++ UF2/00_Repaso_Power_Sehll/16_funciones_parametros.ps1
@@ -0,0 +1,56 @@
+﻿#Ejemplos de funciones 
+
+#parametres
+param(
+        $csv,$log
+      )
+
+function hora ($param1, $param2)
+{
+   <#
+       .SYNOPSIS
+       hora devuelve la hora actual del sistema
+       .DESCRIPTION
+       simplemente llama la funcion 'hora'
+       .EXAMPLE
+       hora num1 num2
+   #>
+   
+   
+   $hora = $(Get-Date).Hour
+   $dia = $param1 -$param2
+   Write-Host "la hroa es $hora y el numero es $dia"
+}
+
+
+
+  Function Sumar ($x, $y) {
+    $sumar = $x + $y
+    Write-Host “La respuesta es $sumar”
+  }
+
+  Function Fecha { Get-Date }
+
+  function tortilla 
+  {
+      param(
+        [Parameter(Mandatory=$true)] [string] $ingr1,
+        [Parameter(Mandatory=$true)] [string] $ingr2,
+        [Parameter(Mandatory=$true)] [string] $ingr3
+      ) 
+
+      Write-Host "La tortilla lleva $ingr1, $ingr2 y $ingr3"
+  }
+  tortilla cebolla patata huevo
+
+  function accion ($param1, $param2)
+{
+    Write-host "estmos dentro de la funcion"
+}
+
+
+Write-host "Este es el CSV $csv y este es el log $log"
+accion $csv $log
+Write-host "Seguimos ejecutando la estructura del script despues de la ejecución de la función"
+Write-host "Si volvemos a invocar la función volverá a ser ejecutada."
+accion 
