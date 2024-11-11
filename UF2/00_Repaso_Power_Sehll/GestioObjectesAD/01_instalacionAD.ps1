789d32b886445a4e139e64c87c029417c85f4fb7
m14 <111636899+ssergivelass@users.noreply.github.com>
2024-11-11 20:25:36 +0100

    Add files via upload

---
+++ UF2/00_Repaso_Power_Sehll/GestioObjectesAD/01_instalacionAD.ps1
@@ -0,0 +1,12 @@
+﻿#Importar el modul neecessari per instal·lar Active Directory amb els seus complements.
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+#Per promocionar el servidor a controlador de domini, primer hem d’importar el mòdul que conté els cmdlets d'Active Directory
+Import-Module ADDSDeployment
+#Instal·lar modul de Install-ADDSForest
+Install-ADDSForest
+
+#Mostrar informació del domini.
+Get-AdDomain
+
+
