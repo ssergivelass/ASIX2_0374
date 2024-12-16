4033efc7a7d790fd426b7f5aa9b0966c156880b0
m14 <111636899+ssergivelass@users.noreply.github.com>
2024-12-16 18:37:42 +0100

    Add files via upload

---
+++ UF2/00_Repaso_Power_Sehll/18_concatenar_&_Split.ps1
@@ -0,0 +1,7 @@
+﻿$nombre = "sergio"
+$cognom =  "velasco"
+$login = "$($nombre[0])$cognom"
+Write-host  $login
+$domini = $(Get-ADDomain).DNSRoot
+$correo = "$nombre.$cognom@$domini"
+Write-host $correo
