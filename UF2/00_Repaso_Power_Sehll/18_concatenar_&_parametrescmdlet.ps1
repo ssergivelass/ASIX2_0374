4783f524157f105a73a1e7824576411e73a8dbaa
m14 <111636899+ssergivelass@users.noreply.github.com>
2024-12-17 08:03:34 +0100

    Update 18_concatenar_&_parametrescmdlet.ps1

--- UF2/00_Repaso_Power_Sehll/18_concatenar_&_parametrescmdlet.ps1
+++ UF2/00_Repaso_Power_Sehll/18_concatenar_&_parametrescmdlet.ps1
@@ -3,6 +3,11 @@ $cognom =  "velasco"
 #amb  [n], escollim la posició del valor de la varibale que volgueml
 $login = "$($nombre[0])$cognom"
 Write-host  $login
+#Podem mostrar i/o emmagatzemar a una variable el resultat d'un cmdlet
+#llistem el resultat d'un cmdlet, en aquest 
+Get-Date | fl *
+#Si ho empaquetem d'aquesta manera podem recollir un valor del cmdlet.
+(Get-Date).Day
 #amb $(Get-ADDomain). escollim el parametre que necessitem, en aquest cas el domin en format dc.dc
 $domini = $(Get-ADDomain).DNSRoot
 $correo = "$nombre.$cognom@$domini"
