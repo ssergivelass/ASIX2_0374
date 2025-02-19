f5470d44c7da80ad7c4ddbe2e27182139c7dcef0
ASIX <111636899+ssergivelass@users.noreply.github.com>
2025-02-19 08:59:43 +0100

    Update 20_Manipulaciónfechas.ps1

--- UF2/00_Repaso_Power_Sehll/20_Manipulaciónfechas.ps1
+++ UF2/00_Repaso_Power_Sehll/20_Manipulaciónfechas.ps1
@@ -1,6 +1,7 @@
 ﻿get-date | fl * # Todos los parámetros de fecha
 (Get-date).DayOfWeek # Dia de la semana en letra
 Get-Date -Day 1 # mostrar el primer dia del mes.
+(Get-Date).AddHours(2) #Añadir dos horas la fecha actual
 (Get-Date).AddDays(1) # Añadimos un  dia a la fecha actual.
 (Get-Date).AddMonths(1) # Añadimos un  dia a la fecha actual.
 (Get-Date).AddDays(-1) #Podemos restar un dia a la fecha actual.
